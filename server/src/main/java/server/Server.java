7368bd3c15630cac39ad18467de39defd14f3d16
SP0-0k <pixl3mast3r@gmail.com>
2025-10-09 10:31:19 -0600

    Added register and delete so tests can run

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,24 +1,44 @@
 package server;
 
+import com.google.gson.Gson;
 import io.javalin.*;
+import io.javalin.http.Context;
+
+import java.util.Map;
 
 public class Server {
 
-    private final Javalin javalin;
+    private final Javalin server;
 
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        server = Javalin.create(config -> config.staticFiles.add("web"));
+        server.delete("db", ctx -> ctx.result("{}"));
+        server.post("user", ctx -> register(ctx));
 
-        // Register your endpoints and exception handlers here.
+        //FIXME: Register your endpoints and exception handlers here.
 
+    }
+
+    //Register Handler
+    private void register(Context ctx) {
+        var serializer = new Gson();
+        String reqJson = ctx.body();
+        var req = serializer.fromJson(reqJson, Map.class);
+
+        // FIXME: Call to the service and register
+
+
+        //FIXME: Generate authToken from service not hardcoded
+        var res = Map.of("username", req.get("username"), "authToken", "yzx");
+        ctx.result(serializer.toJson(res));
     }
 
     public int run(int desiredPort) {
-        javalin.start(desiredPort);
-        return javalin.port();
+        server.start(desiredPort);
+        return server.port();
     }
 
     public void stop() {
-        javalin.stop();
+        server.stop();
     }
 }
