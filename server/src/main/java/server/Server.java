55f0f9729527f6e8e6e257344c52edabd2c5a91c
SP0-0k <pixl3mast3r@gmail.com>
2025-10-14 09:42:44 -0600

    Added an error if a bad request is made

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -8,15 +8,40 @@ import java.util.Map;
 
 public class Server {
 
+    //TODO: Create Service, write two unit tests for every method, one passing on fail
+
     private final Javalin server;
 
     public Server() {
         server = Javalin.create(config -> config.staticFiles.add("web"));
         server.delete("db", ctx -> ctx.result("{}"));
-        server.post("user", ctx -> register(ctx));
+        server.post("user", this::register); //Can be ctx -> register(ctx)
+        server.post("session", this::login);
 
-        //FIXME: Register your endpoints and exception handlers here.
+        //TODO: Register your endpoints and exception handlers here.
 
+    }
+
+    private void login(Context ctx) {
+        var serializer = new Gson();
+        String reqJson = ctx.body();
+        var req = serializer.fromJson(reqJson, Map.class);
+
+        //TODO: Calls to the service and get authToken
+        if (req.containsValue("username") && req.containsValue("password")) {
+
+
+            // TODO: Call to the service and register
+
+
+            //FIXME: Generate authToken from service not hardcoded
+            var res = Map.of("username", req.get("username"), "authToken", "yzx");
+            ctx.result(serializer.toJson(res));
+        } else {
+            var res = Map.of("message", "Error: bad request");
+            ctx.status(400);
+            ctx.result(serializer.toJson(res));
+        }
     }
 
     //Register Handler
@@ -25,13 +50,23 @@ public class Server {
         String reqJson = ctx.body();
         var req = serializer.fromJson(reqJson, Map.class);
 
-        // FIXME: Call to the service and register
+        if (req.containsValue("username") && req.containsValue("password")) {
 
 
-        //FIXME: Generate authToken from service not hardcoded
-        var res = Map.of("username", req.get("username"), "authToken", "yzx");
-        ctx.result(serializer.toJson(res));
+            // TODO: Call to the service and register
+
+
+            //FIXME: Generate authToken from service not hardcoded
+            var res = Map.of("username", req.get("username"), "authToken", "yzx");
+            ;
+            ctx.result(serializer.toJson(res));
+        } else {
+            var res = Map.of("message", "Error: unauthorized");
+            ctx.status(400);
+            ctx.result(serializer.toJson(res));
+        }
     }
+
 
     public int run(int desiredPort) {
         server.start(desiredPort);
